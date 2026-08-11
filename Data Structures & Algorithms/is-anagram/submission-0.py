e50eecae6d600501787a0701afa0280d1b7595c5
Rae-neu <dou.hai@northeastern.edu>
2026-08-11 12:14:27 -0400

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        count = {}
+        
+        for i in s:
+            count[i] = count.get(i, 0) + 1
+        
+        for j in t:
+            count[j] = count.get(j, 0) - 1
+        
+        for val in count.values():
+            if val != 0:
+                return False
+
+        return True
+        
