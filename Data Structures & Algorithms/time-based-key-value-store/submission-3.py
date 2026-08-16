d2a7467d00a9925f15a6edda4c44cb0630cd151d
Rae-neu <dou.hai@northeastern.edu>
2026-08-16 14:09:24 -0400

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,35 @@
+class TimeMap:
+
+    def __init__(self):
+        self.dic = dict()
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.dic:
+            self.dic[key] = []
+        self.dic[key].append((timestamp, value))        
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.dic:
+            return ""
+        
+        ans = self.dic[key]
+        left = 0
+        right = len(ans) - 1
+        res = 0
+        
+        while left <= right:
+            mid = (right + left) // 2
+
+            if ans[mid][0] < timestamp:
+                res = ans[mid][1]
+                left = mid + 1
+
+            elif ans[mid][0] > timestamp:
+                right = mid - 1
+
+            else:
+                return ans[mid][1]
+        
+        return res
+
+        
